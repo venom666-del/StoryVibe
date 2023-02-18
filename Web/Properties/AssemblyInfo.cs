cb369aca78dc28ed21562a4b13367a5eee28e6ee
Asaf Senyor <Asaf.senyor@outlook.co.il>
2023-02-18 16:12:54 +0200

    starting from scratch

--- Web/Properties/AssemblyInfo.cs
+++ Web/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("39dc7d68-f96a-4f44-ba66-7d4570b7eedb")]
+[assembly: Guid("cdb1c69e-a836-4598-a2fe-fd198ee7eb65")]
 
 // Version information for an assembly consists of the following four values:
 //
